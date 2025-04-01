707e5810246104c7c8bdee39019d34eebc22b5bb
Aleksi Backman <bddubbb@protonmail.com>
2025-04-01 20:27:23 +0300

    Indent using spaces

--- scripts/do_shred.sh
+++ scripts/do_shred.sh
@@ -3,13 +3,13 @@
 set -e
 
 main() {
-	local -r disk_name='sda'
-	local -r output_file="output_shred_${disk_name}.txt"
+    local -r disk_name='sda'
+    local -r output_file="output_shred_${disk_name}.txt"
 
-	date &> ${output_file}
-	udevadm info --query=all --name=/dev/${disk_name} | grep ID_SERIAL= &>> ${output_file}
-	shred --verbose --zero /dev/${disk_name} &>> ${output_file}
-	date &>> ${output_file}
+    date &> ${output_file}
+    # udevadm info --query=all --name=/dev/${disk_name} | grep ID_SERIAL= &>> ${output_file}
+    # shred --verbose --zero /dev/${disk_name} &>> ${output_file}
+    date &>> ${output_file}
 }
 
 main "${@}"
