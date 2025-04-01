e983ee0ec98dca72e5e9ae0f52093abd1041e215
Aleksi Backman <bddubbb@protonmail.com>
2025-04-01 20:28:30 +0300

    Exit early on empty disk name

--- scripts/do_shred.sh
+++ scripts/do_shred.sh
@@ -3,8 +3,13 @@
 set -e
 
 main() {
-    local -r disk_name='sda'
+    local -r disk_name=''
     local -r output_file="output_shred_${disk_name}.txt"
+
+    if [ -z "${disk_name}" ] ; then
+        echo "Disk name is empty. Exiting."
+        exit 1
+    fi
 
     date &> ${output_file}
     # udevadm info --query=all --name=/dev/${disk_name} | grep ID_SERIAL= &>> ${output_file}
